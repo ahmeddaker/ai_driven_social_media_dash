58f10d228a4cde0142c0bb853ac68bd9b0be7fd2
ahmeddaker <ahmeddakerwork@gmail.com>
2022-01-28 02:54:39 +0200

    Update simulation_dev.py

--- simulation_dev.py
+++ simulation_dev.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 20:57:41 2022
+@author: Memen
+"""
+
 
 import json
 import time
@@ -5,8 +11,16 @@ import argparse
 import random
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("length", type=int, default=-1,
+                    help="length of array will generate")
+
+args = parser.parse_args()
+lengthOfJson = args.length
+
+
 with open("./tweets.json", 'r', encoding='UTF-8') as f:
-    data = json.load(f)
+    data = json.load(f)[0:lengthOfJson]
 
     steps = 5
     startIndex, endIndex = 0, 0+steps
@@ -18,7 +32,7 @@ with open("./tweets.json", 'r', encoding='UTF-8') as f:
         while(data[startIndex] != None):
             print("********************")
             pre = random.randint(0, len(data))
-            with open("./test-{}.json".format(pre), 'w') as o:
+            with open("/var/log/tweet/test-{}.json".format(pre), 'w') as o:
                 for item in list(data[startIndex:endIndex]):
                     o.write(json.dumps(item)+'\n')
                     print(item)
